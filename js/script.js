f638c7a14073f5a059a7014401384d120cc28e9e
Ivan <exampl@gmail.com>
2020-10-11 16:47:52 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// let incr = 10,decr=10;
+// incr++;//інкремент
+// decr--;//декремент
+// console.log(incr);
+// console.log(decr);
+const isChecked = true;
+isClose = true;
+console.log(isChecked || !isClose);
