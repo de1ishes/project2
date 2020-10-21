aa8e3a18efc30b102d63851a82cbd685e6f6503f
Vadik <exampl@gmail.com>
2020-10-21 19:05:55 +0300

    vadila

--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
 "use strict";
-let num = 20;
-function showFirstMessage(){
-    console.log("hello keks");
-    console.log(num);
-}
-showFirstMessage();
-console.log(num);
+let str = "some";
+let Object = new String(str);
+console.log(typeof(str));
+console.log(typeof(Object));
 
-function calc(a,b){
-    return (a+b);
-}
-console.log(calc(4,5));
-console.log(calc(10,5));
-console.log(calc(20,5));
-
-function ret(){
-let num = 50;
-return num;
-}
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function(){
-    console.log("Hello");
-
-};
-logger();
+console.dir([1,2,3]);
